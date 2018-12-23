8f7ab5ec8ce4a375d5b6d196d71769ef408a3347
Marco <you@example.com>
2018-12-23 18:07:25 -0300

    checkbox fix

--- components/Todo/Item.js
+++ components/Todo/Item.js
@@ -3,24 +3,36 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Container, Header, Content, List, ListItem, Button, Icon, CheckBox, Body} from 'native-base';
 
 export default class Item extends React.Component {
+  states = {
+    checked: false
+  };
 
-  constructor(props){
-    super(props)
+  constructor (props){
+    super (props);
+    this.states.checked = this.props.states;
 
-    this.state = {
-      checked: false,
-      //checked: this.props.item.checked,
-    };
+    this.handleClick = this.handleClick.bind(this);    
   }
 
+  handleClick (){
+    this.setState({
+      checked: !this.states.checked
+    })
+
+    this.states.checked = !this.states.checked
+
+  }
 
   render() {
-            <ListItem>
-              <CheckBox checked={this.state.checked} onPress={() => this.setState({ checked: !this.state.checked })} color="#44f"/>
-              <Body>
-                <Text>&nbsp;&nbsp;&nbsp;{it}</Text>
-              </Body>
-            </ListItem>
+    //let text = this.state.checked ? <strike>{this.props.title}</strike> : this.props.icon;
+    return(
+      <View style={styles}>
+        <CheckBox checked={this.states.checked} onPress={this.handleClick.bind(this)} color="#44f"/>
+        <Body>
+          <Text>&nbsp;&nbsp;&nbsp;{this.props.title}</Text>
+        </Body>
+      </View>
+    );
   }
 }
 
--- components/Todo/index.js
+++ components/Todo/index.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Container, Header, Content, List, ListItem, Button, Icon, CheckBox, Body} from 'native-base';
+import { Container, Header, Content, List, ListItem, Icon, Button} from 'native-base';
 import { reducers } from './reducers';
 import Item from './Item';
 
+
+
 export default class TodoList extends React.Component {
+  
+  constructor (props){
+    super ();
+  }
 
   render() {
-    const items = ['1', '2', '3asdasdawe'];
+
+
+    const list = [
+      {
+        title: 'uno',
+        icon: 'flight-takeoff'
+      },
+      {
+        title: 'dos',
+        icon: 'flight-takeoff'
+      },
+      {
+        title: 'tres',
+        icon: 'flight-takeoff'
+      },
+      {
+        title: 'cuatro',
+        icon: 'flight-takeoff'
+      }
+    ];
+    
+    //let items = ['1', '2', '3asdasdawe'];
     return (
       <Container>
         <Header />
@@ -15,9 +42,17 @@ export default class TodoList extends React.Component {
         <Button>
             <Text>Agregar</Text>
           </Button>
-        <List dataArray={items}
-          renderRow={(it) => <Item item={it}/>  }>
-        </List>
+          <List>
+            {
+              list.map((item) => (
+                <Item
+                  key={item.title}
+                  title={item.title}
+                  leftIcon={{name: item.icon}}
+                />
+              ))
+            }
+          </List>
         </Content>
       </Container>
     );
